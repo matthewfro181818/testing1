9aafe0896c5763e62ac1018b5f49ae9e13295bf5
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-25 13:11:16 -0500

    Create SwfCharacter.hx

---
+++ source/SwfCharacter.hx
@@ -0,0 +1,43 @@
+package;
+
+import openfl.display.Sprite;
+import openfl.display.MovieClip;
+import swf.exporters.animate.AnimateLibrary;
+import openfl.utils.Assets;
+
+class SwfCharacter extends Sprite
+{
+    public var library:AnimateLibrary;
+    public var current:MovieClip;
+    public var currentSymbol:String;
+
+    public function new(lib:AnimateLibrary)
+    {
+        super();
+        this.library = lib;
+    }
+
+    public function play(symbol:String):Void
+    {
+        var clip = library.getMovieClip(symbol);
+
+        if (clip == null)
+        {
+            trace("[SwfCharacter] Symbol not found: " + symbol);
+            return;
+        }
+
+        if (current != null && current.parent == this)
+            removeChild(current);
+
+        current = clip;
+        currentSymbol = symbol;
+        current.gotoAndPlay(1);
+        addChild(current);
+    }
+
+    public function listSymbols():Array<String>
+    {
+        return library.symbols.keys();
+    }
+}
